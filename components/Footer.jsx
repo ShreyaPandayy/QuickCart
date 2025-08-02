ea425aa9aa1d309fd62941e06b052aba5a43d38a
ShreyaPandayy <shreeyapandey456@gmail.com>
2025-08-02 20:19:24 +0530

    third commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -50,14 +50,14 @@ const Footer = () => {
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
-              <p>+91-8759763790</p>
-              <p>contact@upadhyayaman448@gmail.com</p>
+              <p>+91-8756421349</p>
+              <p>contact @shreeyapandey456@gmail.com</p>
             </div>
           </div>
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © AmanKumarUpadhyay All Right Reserved.
+        Copyright 2025 ©ShreyaPandey All Right Reserved.
       </p>
     </footer>
   );
